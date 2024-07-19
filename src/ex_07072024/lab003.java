3c4ef2dbde8cfcd06eb6cdd1985d44baa0749ee3
pavanikothoor <pavanik8615@gmail.com>
2024-07-20 00:12:10 +0530

    new file

--- src/ex_07072024/lab003.java
+++ src/ex_07072024/lab003.java
@@ -4,9 +4,7 @@ public class lab003 {
         public static void main(String[] args) {
 
             // Package -> Class -> Main Method -> statements(instructions)
-            System.out.println(2+2);
-            System.out.println(2*2);
-
+            System.out.println(2 + 2);
+            System.out.println(2 * 2);
         }
-    }
-
+        }
