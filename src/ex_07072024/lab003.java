687e658141dfe51ecb418bb9ea0bb8a7f321743d
pavanikothoor <pavanik8615@gmail.com>
2024-07-19 23:51:54 +0530

    new files are added

--- src/ex_07072024/lab003.java
+++ src/ex_07072024/lab003.java
@@ -6,7 +6,6 @@ public class lab003 {
             // Package -> Class -> Main Method -> statements(instructions)
             System.out.println(2+2);
             System.out.println(2*2);
-            System.out.println(3/2); // Integer
 
         }
     }
