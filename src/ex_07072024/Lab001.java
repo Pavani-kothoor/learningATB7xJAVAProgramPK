5b3d4ac84a2ba759542dee5c673ac59ec600bd19
pavanikothoor <pavanik8615@gmail.com>
2024-07-19 22:02:10 +0530

    new files are added

---
+++ src/ex_07072024/Lab001.java
@@ -0,0 +1,9 @@
+package ex_07072024;
+
+public class Lab001 {
+    public static void main(String[] args){
+
+    }
+}
+
+
---
+++ src/ex_07072024/lab002.java
@@ -0,0 +1,7 @@
+package ex_07072024;
+
+public class lab002 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
