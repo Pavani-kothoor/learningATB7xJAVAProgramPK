2c7f73fefc380347edf220a66696176ea24edd12
pavanikothoor <pavanik8615@gmail.com>
2024-07-20 17:58:13 +0530

    file added

---
+++ src/ex_07072024/lab004.java
@@ -0,0 +1,15 @@
+package ex_07072024;
+
+public class lab004 {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(0);
+        System.out.println(2*2);
+        System.out.println(1);
+        System.out.println(3/2);
+       }
+
+
+}
+
+
